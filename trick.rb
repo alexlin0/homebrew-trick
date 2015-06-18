bd1adf7076f24326f0c7370b84aa4b71324a4a6f
Alex Lin <alexander.s.lin@nasa.gov>
2015-06-18 13:41:32 -0500

    cleaned up the formula

--- trick.rb
+++ trick.rb
@@ -1,6 +1,5 @@
 # Documentation: https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md
 #                /usr/local/Library/Contributions/example-formula.rb
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 
 class Trick < Formula
   desc ""
@@ -9,8 +8,6 @@ class Trick < Formula
   version "test"
   sha256 ""
 
-  # depends_on "cmake" => :build
-  depends_on "brew-cask"
   depends_on :java
   depends_on :x11
   depends_on "llvm" => "with-clang"
@@ -40,15 +37,6 @@ class Trick < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test trick`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "true"
   end
 end
